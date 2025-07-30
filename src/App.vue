83ca3709c09c0f751f4f97e0fec66643918608cf
Rhythm Dubey <rhythm@itradicals.com>
2025-07-30 16:52:13 +0530

    Vue JS project setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const name = ref('Sachin')
+
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <div>Hello, {{ name }}</div>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
