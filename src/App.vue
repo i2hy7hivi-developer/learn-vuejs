2124169bceb8098040806b6adca52eec2b813a3a
Rhythm Dubey <rhythm@itradicals.com>
2025-08-07 13:26:02 +0530

    Form input binding
    Two-Way Data Binding with List Rendering and Event Handling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
 
-import Counter from './components/Counter.vue'
-import Message from './components/Message.vue'
-import Project from './components/Project.vue'
-import Welcome from './components/Welcome.vue'
+import ProjectManager from './components/ProjectManager.vue'
 
 </script>
 
 <template>
-  <Counter />
+  <ProjectManager />
 </template>
 
 <style scoped></style>
---
+++ src/components/ProjectManager.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const newProject = ref('')
+const projects = ref(['MyRentalSpot', 'Rentler', 'Imove360']);
+
+function addProject () {
+    if (newProject.value.trim()) {
+        projects.value.push(newProject.value)
+        newProject.value = ''
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <h4>Projects</h4>
+        <ul>
+            <li v-for="(project, index) in projects" :key="index">{{ project }}</li>
+        </ul>
+
+        <input v-model="newProject" placeholder="Enter New Project" />
+        <button @click="addProject">Add</button>
+    </div>
+</template>
