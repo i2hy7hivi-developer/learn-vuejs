5377d7c7e3726a363782c168be8ba3a6d83f9ced
Rhythm Dubey <rhythm@itradicals.com>
2025-08-07 14:04:54 +0530

    Simple error handling

--- src/components/ProjectManager.vue
+++ src/components/ProjectManager.vue
@@ -2,14 +2,23 @@
 
 import { ref } from 'vue'
 
+const error = ref('')
 const newProject = ref('')
 const projects = ref(['MyRentalSpot', 'Rentler', 'Imove360']);
 
 function addProject () {
+    
+    if (! newProject.value.trim()) {
+        error.value = 'Project name is required'
+        return
+    }
+
     if (newProject.value.trim()) {
         projects.value.push(newProject.value)
-        newProject.value = ''
     }
+    
+    error.value = ''
+    newProject.value = ''
 }
 
 </script>
@@ -20,6 +29,8 @@ function addProject () {
         <ul>
             <li v-for="(project, index) in projects" :key="index">{{ project }}</li>
         </ul>
+
+        <p v-if="error" style="color: red;">{{ error }}</p>
 
         <input v-model="newProject" placeholder="Enter New Project" />
         <button @click="addProject">Add</button>
